3bce468e13cdb87a3e25b2c1db30d83ae075218a
Yaroslav <serov.m1976@mail.ru>
2016-03-20 18:39:57 +0300

    Snake3D
    First step.

---
+++ Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+//прототип движения змейки, по осям x,y,z
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    //скорость по умолчанию
+    public float speed = 8;
+    private CharacterController _controller;
+
+    public void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    public void Update()
+    {
+        float y = 0;
+        float x = 0;
+        float z = 0;                      
+                                                                           
+        if (Input.GetKey(KeyCode.W)) z = 1;
+        if (Input.GetKey(KeyCode.S)) z = -1;
+
+        if (Input.GetKey(KeyCode.UpArrow)) y = 1;
+        if (Input.GetKey(KeyCode.DownArrow)) y = -1;
+
+        if (Input.GetKey(KeyCode.RightArrow)) x = -1;
+        if (Input.GetKey(KeyCode.LeftArrow)) x = 1;
+
+        _controller.Move(transform.forward * speed * Time.deltaTime * y);
+        _controller.Move(transform.right * speed * Time.deltaTime * x);
+        _controller.Move(transform.up * speed * Time.deltaTime * z);
+    }
+}
